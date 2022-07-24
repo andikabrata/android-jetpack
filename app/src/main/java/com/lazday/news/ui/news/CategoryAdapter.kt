0567ea3b2d78fb58fb3f3e6f362cc5bf1b5eee2a
andikabrata <andika.juliansyah@mncgroup.com>
2022-07-24 16:01:45 +0700

    12. Membuat Category Adapter

---
+++ app/src/main/java/com/lazday/news/ui/news/CategoryAdapter.kt
@@ -0,0 +1,48 @@
+package com.lazday.news.ui.news
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.lazday.news.R
+import com.lazday.news.data.news.CategoryModel
+import com.lazday.news.databinding.AdapterCategoryBinding
+
+class CategoryAdapter(
+    val categories: List<CategoryModel>,
+    val listener: OnAdapterListener
+) : RecyclerView.Adapter<CategoryAdapter.ViewHolder>() {
+
+    private val items = arrayListOf<TextView>()
+
+    class ViewHolder(val binding: AdapterCategoryBinding) : RecyclerView.ViewHolder(binding.root)
+
+    interface OnAdapterListener {
+        fun onClick(category: CategoryModel)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        AdapterCategoryBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+    )
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val category = categories[position]
+        holder.binding.category.text = category.name
+        items.add(holder.binding.category)
+        holder.itemView.setOnClickListener {
+            listener.onClick(category)
+            setColor(holder.binding.category)
+        }
+        setColor(items[0]) /// first load
+    }
+
+    override fun getItemCount() = categories.size
+
+    private fun setColor(textView: TextView) {
+        items.forEach { it.setBackgroundResource(R.color.white) }
+        textView.setBackgroundResource(android.R.color.darker_gray)
+    }
+}
